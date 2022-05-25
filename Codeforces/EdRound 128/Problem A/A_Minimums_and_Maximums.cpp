1b85f09fc03aa480ed6384589f5d51d9db6d5cc8
Panos Paskalis <panos.paskalis@gmail.com>
2022-05-25 19:20:20 +0300

    solved some problems

--- Codeforces/EdRound 128/Problem A/A_Minimums_and_Maximums.cpp
+++ Codeforces/EdRound 128/Problem A/A_Minimums_and_Maximums.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
+#define mp make_pair
+
+typedef pair<int,int> ii;
+
+int find_common_range(ii a, ii b)
+{
+    if (a.first <= b.second && b.first <= a.second) {
+        return max(a.first, b.first);
+    }
+
+    return -1;
+}
+
 int main(void)
 {
-    cout << 1;
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        int x1, x2, y1, y2;
+        scanf("%d %d %d %d", &x1, &x2, &y1, &y2);
+
+        int common = find_common_range(mp(x1, x2), mp(y1, y2));
+
+        if (common == -1)
+            common = x1 + y1;
+        
+        printf("%d\n", common);
+    }
 }
