d541224ff57706292bdd72a9bf41814221fa59e2
Panos Paskalis <panos.paskalis@gmail.com>
2022-12-24 12:01:56 +0200

    template ready

---
+++ Usaco_Contest/probname.cpp
@@ -0,0 +1,17 @@
+/*
+ID: panos.p1
+LANG: C++11
+TASK: probname
+*/
+
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+int main(void)
+{
+    freopen("probname.in", "r", stdin);
+    freopen("probname.out", "w", stdout);
+}
