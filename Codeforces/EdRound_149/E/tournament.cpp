db1a32e61472442584e011d36e4ca47349cca737
Panos Paskalis <panos.paskalis@gmail.com>
2023-05-27 15:12:36 +0300

    problem E done

--- Codeforces/EdRound_149/E/tournament.cpp
+++ Codeforces/EdRound_149/E/tournament.cpp
@@ -7,27 +7,21 @@ typedef long long ll;
 typedef pair<int, int> pi;
 
 int k, n;
-vector<int> possible_teams;
-vector<pi> seeds;
+vector<int> cur_round;
+vector<int> next_round;
 vector<int> places_filled;
 vector<ll> fact;
 
-int msb(int i)
+bool must_lose(int i, int lvl)
 {
-    if (i == 0)
-        return 0;
-    else
-        return 32 - __builtin_clz(i);
+    int minimum_lose = (1<<(k-lvl-1));
+    return i >= minimum_lose;
 }
 
-bool isless(int a, int b)
+void impossible(void)
 {
-    return msb(a) < msb(b);
-}
-
-bool customsort(const pi& a, const pi& b)
-{
-    return a.first > b.first;
+    printf("0\n");
+    exit(EXIT_SUCCESS);
 }
 
 int main(void)
@@ -40,50 +34,73 @@ int main(void)
     for (int i = 1; i < fact.size(); i++)
         fact[i] = fact[i - 1] * i % MOD;
 
-    places_filled.assign(n + 1, -1);
-    possible_teams.resize(n);
-
-    possible_teams[0] = 1;
-    for (int i = 1; i < k; i++)
-        possible_teams[i] = 1 << (i - 1);
-
+    cur_round.resize(n);
     for (int i = 0; i < n; i++) {
-        int team;
-        scanf("%d", &team);
-        team--;
-        if (team != -1)
-            seeds.push_back(make_pair(team, i));
+        scanf("%d", &cur_round[i]);
+        if (cur_round[i] != -1)
+            cur_round[i]--;
     }
 
-    sort(seeds.begin(), seeds.end(), customsort);
+    long long ans = 1;
+    for (int r = 0; r < k; r++) {
+        // The r-th round
+        int lose_choice = 0;
+        int advance;
+        for (int i = 0; i < cur_round.size(); i += 2) {
+            // Examine i and i + 1 as they will fight
+            if (cur_round[i] != -1 && cur_round[i + 1] != -1) {
+                bool first_lose = must_lose(cur_round[i], r);
+                bool sec_lose = must_lose(cur_round[i + 1], r);
 
-    for (int i = 0; i < seeds.size(); i++) {
-        int place, team;
-        tie(team, place) = seeds[i];
-        if (places_filled[place] > team) {
-            printf("0\n");
-            return 0;
+                if (first_lose == sec_lose)
+                    impossible();
+
+                if (!first_lose) {
+                    swap(first_lose, sec_lose);
+                    swap(cur_round[i], cur_round[i + 1]);
+                }
+
+                // Cannot pick loser, cannot pick order
+                advance = cur_round[i + 1];
+            }
+            else {
+                if (cur_round[i + 1] != -1)
+                    swap(cur_round[i], cur_round[i + 1]);
+
+                if (cur_round[i] != -1) {
+                    // Determine if i wins or loses and act accordingly
+                    if (must_lose(cur_round[i], r)) {
+                        // Loser is decided, => Cannot change the pair order
+                        // and cannot pick loser
+
+                        advance = -1;
+                    }
+                    else {
+                        // Winner is decided => Cannot change the pair order
+                        // but can pick loser
+
+                        lose_choice++;
+                        advance = cur_round[i];
+                    }
+                }
+                else {
+                    lose_choice++;
+
+                    // Free to choose the order of winner-loser
+                    ans = ans * 2 % MOD;
+                    advance = -1;
+                }
+            }
+
+            next_round.push_back(advance);
         }
-        else {
-            int fill_places = (1 << (k - msb(team)));
 
-            int startpos;
-            if (place < fill_places)
-                startpos = 0;
-            else
-                startpos = place - place % (1 << (msb(fill_places)-1));
-
-            for (int i = startpos; i < startpos + fill_places; i++)
-                places_filled[i] = max(places_filled[i], team);
-
-            possible_teams[msb(team)] = max(possible_teams[msb(team)] - 1, 1);
-        }
-    }
-
-    ll ans = 1;
-    for (int i = 0; i < k; i++) {
-        ans = ans * fact[possible_teams[i]] % MOD;
+        ans = ans * fact[lose_choice] % MOD;
+        swap(next_round, cur_round);
+        next_round.clear();
     }
 
     printf("%lld\n", ans);
+
+    return 0;
 }
