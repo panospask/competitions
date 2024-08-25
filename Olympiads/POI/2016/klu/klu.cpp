0300d12223f6930c66358895f767435a09c1ec10
panospask <panos.paskalis@gmail.com>
2024-08-25 17:12:26 +0300

    upsolve POI problems

--- Olympiads/POI/2016/klu/klu.cpp
+++ Olympiads/POI/2016/klu/klu.cpp
@@ -1,69 +1,163 @@
 #include <bits/stdc++.h>
 #define CHECK_BIT(var, pos) ((var) & (1 << (pos)))
+#define mp make_pair
+#define pb push_back
 
 using namespace std;
 
+typedef pair<int, int> pi;
+
 int N;
-vector<vector<int>> adj_list;
-vector<int> couple;
-vector<int> pos;
 
-bool brute(int node, int p)
+// match[level][i]: The assigned match for i for every level of the recursion
+vector<vector<int>> match;
+vector<vector<int>> pos;
+
+void assign_positions(vector<int>& c, int l)
 {
-    if (pos[node] != -1) {
-        return false;
+    for (int i = 0; i < c.size(); i++) {
+        pos[l][c[i]] = i;
     }
+}
 
-    pos[node] = p;
-    if (p == (1 << N) - 1) {
-        return __builtin_popcount(node) == 1;
+bool is_solo(int i, int l)
+{
+    return match[l][i] != match[l + 1][i];
+}
+
+// Returns true if there exists a fake match between i and j at level l
+bool is_fake(int i, int j, int l)
+{
+    return match[l][i] == j && is_solo(i, l - 1);
+}
+
+// Given a set of 2^i nodes and a perfect matching between them, turn it into a cycle
+// using the rules in the problem statement
+vector<int> compute(vector<int> nums, int level)
+{
+    if (nums.size() == 2) {
+        return nums;
     }
+    // Split the nodes into 2 disjoint sets and solve recursively
 
-    for (int b = 0; b < N; b++) {
-        int neigh = node ^ (1 << b);
-        if (pos[neigh] == -1) {
-            pos[neigh] = p + 1;
-            if (brute(couple[neigh], p + 2)) {
-                return true;
-            }
-            pos[neigh] = -1;
+    // Ensure there is always at least one edge of the matching between the two sides
+    int split = -1;
+    for (int b = 0; b < N && split == -1; b++) {
+        if (CHECK_BIT(nums[0], b) ^ CHECK_BIT(match[level][nums[0]], b)) {
+            split = b;
         }
     }
 
-    pos[node] = -1;
+    // Split and create arbitrary fake edges for the first half
+    vector<int> n[2];
+    int prv = -1;
+    int fake_count = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        bool v1 = CHECK_BIT(nums[i], split);
+        bool v2 = CHECK_BIT(match[level][nums[i]], split);
 
-    return false;
+        n[v1].pb(nums[i]);
+        if (v2 == v1) {
+            match[level + 1][nums[i]] = match[level][nums[i]];
+        }
+        else if (!v1) {
+            // We are at the first half
+            if (prv != -1) {
+                // Create fake edge
+                match[level + 1][nums[i]] = prv;
+                match[level + 1][prv] = nums[i];
+                prv = -1;
+                fake_count++;
+            }
+            else {
+                prv = nums[i];
+            }
+        }
+    }
+
+    auto r1 = compute(n[0], level + 1);
+    assign_positions(r1, level + 1);
+
+    // Create the fake edges for the second half by uniting matches of nodes of the first half that are connected by REAL edges only
+
+    prv = -1;
+    for (int i = 0; i < r1.size(); i++) {
+        if (is_solo(r1[i], level)) {
+            if (prv != -1 && !is_fake(prv, r1[i], level + 1)) {
+                match[level + 1][match[level][r1[i]]] = match[level][prv];
+                match[level + 1][match[level][prv]] = match[level][r1[i]];
+                prv = -1;
+            }
+            else {
+                prv = r1[i];
+            }    
+        }
+    }
+    if (prv != -1) {
+        int i = 0;
+        while (!is_solo(r1[i], level)) {
+            i++;
+        }
+
+        match[level + 1][match[level][r1[i]]] = match[level][prv];
+        match[level + 1][match[level][prv]] = match[level][r1[i]];
+    }
+
+    auto r2 = compute(n[1], level + 1);
+    if (is_fake(r2.front(), r2.back(), level + 1)) {
+        r2.insert(r2.begin(), r2.back());
+        r2.pop_back();
+    }
+    assign_positions(r2, level + 1);
+
+    vector<int> tot;
+    tot.pb(r2.front());
+
+    for (int i = 1; i < r2.size(); i++) {
+        if (is_fake(r2[i - 1], r2[i], level + 1)) {
+            int p = pos[level + 1][match[level][r2[i - 1]]];
+            int mod = 1;
+            if (is_fake(r1[p], r1[(p + 1) % r1.size()], level + 1)) {
+                mod = -1;
+            }
+
+            for (int j = p; r1[j] != match[level][r2[i]]; j = (j + mod + r1.size()) % r1.size()) {
+                tot.pb(r1[j]);
+            }
+            tot.pb(match[level][r2[i]]);
+        }
+
+        tot.pb(r2[i]);
+    }
+
+    return tot;
 }
 
 int main(void)
 {
     scanf("%d", &N);
 
-    adj_list.resize(1 << N);
-    couple.resize(1 << N);
-    pos.assign(1 << N, -1);
+    pos.assign(N, vector<int>(1 << N, 0));
+    match.assign(N, vector<int>(1 << N, 0));
+
+    vector<int> init;
 
     for (int i = 0; i < 1 << (N - 1); i++) {
         int u, v;
         scanf("%d %d", &u, &v);
-        couple[u] = v;
-        couple[v] = u;
+        match[0][u] = v;
+        match[0][v] = u;
+
+        init.pb(u);
+        init.pb(v);
     }
 
-    pos[0] = 0;
-    if (brute(couple[0], 1) == false) {
-        printf("NIE\n");
-        return 0;
-    }
+    auto ans = compute(init, 0);
 
-        vector<int> a(1 << N);
-        for (int i = 0; i < (1 << N); i++) {
-            a[pos[i]] = i;
-        }
-        for (int i = 0; i < (1 << N); i++) {
-            printf("%d ", a[i]);
-        }
-        printf("\n");
+    for (auto u : ans) {
+        printf("%d ", u);
+    }
+    printf("\n");
 
     return 0;
 }
--- Olympiads/POI/mix/cyk/cyk.cpp
+++ Olympiads/POI/mix/cyk/cyk.cpp
@@ -7,52 +7,105 @@ using namespace std;
 typedef long long ll;
 
 const ll INF = 1e18 + 1;
-const int MAXLEN = 100;
 
-// A[i]: Value at position 2^i
-vector<ll> A;
+ll PREVIOUS_ELEMENT = -1;
 
-// pos[i]: If length is i, what position are we at?
-vector<int> pos;
-
-void update_position(int l, int r, int steps)
+/* Returns true iff x is bigger than the length of the sequence
+ * It is necessary that:
+ * x / 2 <= len < 2 * x 
+ * for the function to work
+ */
+bool bigger(int x)
 {
-	for (int i = l; i < r; i++) {
-		pos[i] = (pos[i] + steps) % i;
+	int first = x / 2;
+	int sec = x - first;
+
+	ll r1 = PREVIOUS_ELEMENT;
+	ll r2 = circular_shift(first);
+	ll r3 = circular_shift(sec);
+
+	// How many times did the sequence circle fron the last element to the first?
+	int times = (r1 >= r2) + (r2 >= r3);
+
+	assert(times >= 0 && times <= 2);
+
+	PREVIOUS_ELEMENT = r3;
+
+	if (times == 2) {
+		// We covered a distance greater than len by moving x, therefore x is greater than len
+		return true;
+	}	
+	else if (times == 1) {
+		// Only circled back to the start once.
+		return r3 > r1;
+	}
+	else {
+		// We have not even circled back once, therefore x is less than the length of the sequence
+		return false;
 	}
 }
 
 int main(void) 
 {
 	int minlen = 1;
-	A.pb(circular_shift(0));
+	ll prv = circular_shift(0);
+	ll first = prv;
+	ll cur;
+
+	int l, r;
 
 	while (true) {
-		A.pb(circular_shift(minlen));
-		update_position(minlen, MAXLEN, minlen);
+		cur = circular_shift(minlen);
 
-		if (A.back() > A[A.size() - 2]) {
+		if (cur > prv) {
 			minlen *= 2;
+			prv = cur;
 		}
 		else {
 			break;
 		}
 	}
 
-	ll landed = A.back();
-	A.pop_back();
+	if (cur < first) {
+		int x = minlen;
+		minlen = 1;
+		prv = cur;
 
-	int bef = lower_bound(A.begin(), A.end(), landed) - A.begin();
-	int l_len = 2 * minlen - (1 << bef);
-	if (A[bef] == landed) {
-		give_answer(l_len);
+		while (true) {
+			cur = circular_shift(minlen);
+
+			if (cur > prv) {
+				minlen *= 2;
+				prv = cur;
+			}
+			else {
+				break;
+			}
+		}
+
+		// Since x < minlen we gather that l >= 3r, hence for all the queries bigger(mid) the condition is fullfilled.
+		l = minlen;
+		r = 2 * minlen + x;
 	}
-	int r_len = 2 * minlen - (1 << (bef - 1));
+	else {
+		l = minlen;
+		r = 2 * minlen + 1;
+	}
 
-	int l_pos = 1 << (bef - 1);
-	int r_pos = 1 << bef;
+	PREVIOUS_ELEMENT = cur;
 
+	// The sequence has size greater than or equal to l and smaller than r
+	while (l + 1 < r) {
+		int mid = (l + r) / 2;
+		if (bigger(mid)) {
+			r = mid;
+		}
+		else {
+			l = mid;
+		}
+	}
 
+	give_answer(l);
 
 	return 0;
 }
